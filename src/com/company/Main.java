9806b43e91e8a3bc620c8693d9db0dbb877ef8de
Nguyen Huy Hoang <huyhoang681997@gmail.com>
2018-08-17 19:04:32 +0700

    Hoang finished Hello Machine Project

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Insert your name");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Hello: "+name);
+    }
+}
